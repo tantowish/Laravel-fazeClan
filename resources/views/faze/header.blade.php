2e5b2ef9b383c031565eca19f993387b073e0efd
tanto <tantows001@gmail.com>
2023-09-07 14:26:29 +0700

    first commit

--- resources/views/faze/header.blade.php
+++ resources/views/faze/header.blade.php
@@ -1,4 +1,4 @@
-<div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+<div class="px-4 mx-auto max-w-screen-xl text-center py-12 lg:py-24">
     <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">World number one esport clan</h1>
     <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">FaZe Clan is an American professional esports and entertainment organization founded in May 2010. They are the first international team to win a Global Offensive Major.</p>
     <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
